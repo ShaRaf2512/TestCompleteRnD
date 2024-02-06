ccc684fb0eb3978114261cfa28e0c6d5ae7af307
Sadaf Abdulsattar Shaikh <ssadaf@enhops.com>
2024-02-06 17:15:26 +0530

    curl command test

--- testZephyr.bat
+++ testZephyr.bat
@@ -6,6 +6,6 @@ CLS
 REM Launches TestExecute
 REM executes the specified project
 REM and closes TestExecute when the run is over
-curl "https://support.smartbear.com/testcomplete/docs/working-with/integration/jenkins/send-test-results-to-zephyr.html"
+curl "https://plugins.jenkins.io/zephyr-enterprise-test-management/"
 
 :OkEnd
