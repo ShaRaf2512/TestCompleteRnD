7bf6b48f71f6aa8c3849712fc73e8c293779405b
Sadaf Abdulsattar Shaikh <ssadaf@enhops.com>
2024-02-06 16:55:52 +0530

    curl command test

---
+++ testZephyr.bat
@@ -0,0 +1,11 @@
+REM Clears the screen
+CLS
+@ECHO OFF
+
+:ExecuteTest
+REM Launches TestExecute
+REM executes the specified project
+REM and closes TestExecute when the run is over
+curl "https://support.smartbear.com/testcomplete/docs/working-with/integration/jenkins/send-test-results-to-zephyr.html"
+
+:OkEnd
